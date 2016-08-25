b20f257ded55508d07dc5842f4751902c53a78c9
Victorien D <Victorien D>
2016-08-25 15:34:30 +0200

    Initial commit

---
+++ public/javascripts/camera.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Victorien on 24-06-16.
+ */
+//var socket = io.connect('http://localhost:3000');
+const exec = require('child_process').exec;
+const getSerial = "cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2";
+
+connectServer();
+
+//Events
+
+socket.on('timer', function(data){
+    console.log("on timer");
+    setTimer(data.begin_hour, data.begin_minute, data.end_hour, data.end_minute, data.frequency);
+    console.log("On timer ok");
+});
+
+//Functions-----------------------------
+
+
+function connectServer(){
+    exec(getSerial, function(error, stdout, stderr){
+        if(error){
+            console.log('error : '+ error);
+        }
+        console.log(stdout);
+        socket.emit('camera', stdout);
+    });
+}
+
+function setTimer(beginHour, beginMinute, endHour, endMinute, frequency){
+    console.log('in function setTimer :');
+    //get time to record
+    var timeRecord = (((endHour*3600)+(endMinute*60))-((beginHour*3600)+(beginMinute*60)));
+    //get camera name
+    var cameraName = 'cuisine';
+    //get frequency for cron
+    var freq = "";
+    switch(frequency) {
+        case 'mon':
+            freq = "1";
+            break;
+        case 'tue':
+            freq = "2";
+            break;
+        case 'wed':
+            freq = "3";
+            break;
+        case 'thu':
+            freq = "4";
+            break;
+        case 'fri':
+            freq = "5";
+            break;
+        case 'sat':
+            freq = "6";
+            break;
+        case 'sun':
+            freq = "7";
+            break;
+        default:
+            freq = "*";
+    }
+
+    var cron = beginMinute+" "+beginHour+" * * "+freq+" pi ";
+    var cmdPython = "python /home/pi/TFE/python/record/record.py -c /home/pi/TFE/python/record/conf.json -t "+time$
+    var cmd = "echo '"+cron+cmdPython+"' > /etc/cron.d/record";
+    console.log(cmd);
+    console.log("before exec");
+    exec(cmd, function(error, stdout, stderr) {
+        if(error){
+            console.log(error);
+        }
+        console.log(stdout);
+    });
+
+    console.log("after exec");
+}
+
+
